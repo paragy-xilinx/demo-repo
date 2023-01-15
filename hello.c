e39fbc9cac660e92f991589a716c5fc035b0d37b
Parag Yadav <paragy@xhdparagy41x.xlnx.xilinx.com>
2023-01-15 14:11:44 +0530

    hello: created new file added new file hello.c in branch 1

---
+++ hello.c
@@ -0,0 +1,11 @@
+/* this is the required header file */
+#include<stdio.h>
+
+/* this is main function which OS will called */
+int main()
+{
+/*using printf API */
+printf("Hello from branch 1\n");
+
+return 0;
+}
